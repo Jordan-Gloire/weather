bf0f4720e637a48b720b96a6d9e7892506485c69
Jordan Gloire <132207668+Jordan-Gloire@users.noreply.github.com>
2024-03-31 18:17:13 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/60 z-[1] flex justify-center items-center flex-col">
-       <p className=" text-white text-xl text-start py-4 px-8 ">Hydratez-vous régulièrement, Évitez les heures les plus chaudes, Utilisez un ventilateur ou la climatisation,  Prenez des douches fraîches, Évitez les endroits surpeuplés...</p>
+       <p className=" text-white text-xl text-start py-4 px-8 ">Hydratez-vous régulièrement, Évitez les heures les plus chaudes, Utilisez un ventilateur ou la climatisation,  Prenez des douches fraîches, Évitez les endroits surpeuplés.</p>
        
         <Link href={`/Meteo`} className="bg-cyan-600 mt-8 text-white hover:text-black duration-700 ease-in rounded-md outline-none border-none py-2 px-6 text-lg">Voir la météo</Link>
       </div>
