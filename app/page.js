89f3a01baf1cb90c7fb9f1a523043f24e55bc51f
Jordan Gloire <132207668+Jordan-Gloire@users.noreply.github.com>
2024-04-01 05:31:03 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -23,8 +23,38 @@ const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric
   return (
     <>
       <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/60 z-[1] flex justify-center items-center flex-col">
-       <p className=" text-white text-xl text-start py-4 px-8 ">"Hydratez-vous régulièrement, Évitez les heures les plus chaudes, Utilisez un ventilateur ou la climatisation,  Prenez des douches fraîches, Évitez les endroits surpeuplés..."</p>
-       
+        <Carousel autoplay className="w-screen mx-8 text-white text-2xl">
+            <div>
+            <h3 style={contentStyle}>
+                Restez hydraté
+              Évitez les activités physiques intenses
+                
+              Utilisez un écran solaire
+              </h3>
+            </div>
+            <div>
+            <h3 style={contentStyle}>
+                            Portez des vêtements légers 
+
+              Mangez léger
+              Surveillez les signes de coup de chaleur 
+              </h3>
+            </div>
+           
+            <div>
+             <h3 style={contentStyle}>
+              Évitez les heures les plus chaudes
+              Utilisez un ventilateur ou la climatisation
+              </h3>
+            </div>
+             <div>
+            <h3 style={contentStyle}>
+            Pour plus d'astuces
+              consultez un professionnel de santé.
+              </h3>
+            </div>
+        </Carousel>
+       </div>
         <Link href={`/Meteo`} className="bg-cyan-600 mt-8 text-white hover:text-black duration-700 ease-in rounded-md outline-none border-none py-2 px-6 text-lg">Voir la météo</Link>
         <span className="loading loading-ring loading-lg"></span>
       </div>
