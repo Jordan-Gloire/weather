b70ca75ca979c91a1055c51bb979fa82ea0b43ff
Jordan Gloire <132207668+Jordan-Gloire@users.noreply.github.com>
2024-04-02 12:36:27 +0200

    modif indente page.js

--- app/page.js
+++ app/page.js
@@ -22,44 +22,39 @@ const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric
 };
   return (
     <>
-            <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/60 z-[1] flex justify-center items-center flex-col">
-            <p className="text-6xl text-white font-bold">{formattedTime}</p>
-              <Carousel autoplay className="w-screen mx-8 text-white text-2xl">
-                  <div>
-                  <h3 style={contentStyle}>
-                      Restez hydraté
-                    Évitez les activités physiques intenses
-                      
-                    Utilisez un écran solaire
-                    </h3>
-                  </div>
-                  <div>
-                  <h3 style={contentStyle}>
-                                  Portez des vêtements légers 
-      
-                    Mangez léger
-                    Surveillez les signes de coup de chaleur 
-                    </h3>
-                  </div>
-                 
-                  <div>
-                   <h3 style={contentStyle}>
-                    Évitez les heures les plus chaudes
-                    Utilisez un ventilateur ou la climatisation
-                    </h3>
-                  </div>
-                   <div>
-                  <h3 style={contentStyle}>
-                  Pour plus d'astuces
-                    consultez un professionnel de santé.
-                    </h3>
-                  </div>
-              </Carousel>
-             </div>
-              <Link href={`/Meteo`} className="bg-cyan-600 mt-8 text-white hover:text-black duration-700 ease-in rounded-md outline-none border-none py-2 px-6 text-lg">Voir la météo</Link>
-        
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/60 z-[1] flex justify-center items-center flex-col">
+        <p className="text-6xl text-white font-bold">{formattedTime}</p>
+        <p className="text-white py-3">{dateFormatee}</p>
+         <Carousel autoplay className="w-screen text-white text-2xl">
+            <div>
+            <h3 style={contentStyle}>
+                Restez hydraté
+              Évitez les activités physiques intenses
+              </h3>
             </div>
-      </>
-    
+            <div>
+            <h3 style={contentStyle}>
+              Portez des vêtements légers  <br/>
+              Mangez léger
+              </h3>
+            </div>
+           
+            <div>
+             <h3 style={contentStyle}>
+              Évitez les heures les plus chaudes
+              Utilisez un ventilateur/climatisation
+              </h3>
+            </div>
+             <div>
+            <h3 style={contentStyle}>
+            Pour plus d'astuces
+              consultez un professionnel de santé.
+              </h3>
+            </div>
+        </Carousel>
+        <Link href={`/Meteo`} className="bg-cyan-600 mt-8 text-white hover:text-black duration-700 ease-in rounded-md outline-none border-none py-2 px-6 text-lg">Voir la météo</Link>
+      </div>
+      <Image src={imgdeFond} className="h-screen w-screen object-cover"/>
+    </>
   );
 }
